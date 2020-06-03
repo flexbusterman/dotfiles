4169f3e4aa0e96e63de1ab8a8b0ace97cbbdec1f
flexbusterman <generalstrejk@gmail.com>
2020-06-03 17:45:05 +0200

    smaller font

--- .local/src/dwm/config.h
+++ .local/src/dwm/config.h
@@ -11,8 +11,8 @@ static const int smartgaps          = 1;        /* 1 means no outer gap when the
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=14", "JoyPixels:pixelsize=14:antialias=true:autohint=true"  };
-static char dmenufont[]             = "monospace:size=14";
+static const char *fonts[]          = { "monospace:size=13", "JoyPixels:pixelsize=13:antialias=true:autohint=true"  };
+static char dmenufont[]             = "monospace:size=13";
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
 static char normfgcolor[]           = "#bbbbbb";
