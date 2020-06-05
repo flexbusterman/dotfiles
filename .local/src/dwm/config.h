fa292a3b140e3d5b7976bce6e7fc7048a5fe422a
flexbusterman <generalstrejk@gmail.com>
2020-06-05 22:56:05 +0200

    mpv on all tags

--- .local/src/dwm/config.h
+++ .local/src/dwm/config.h
@@ -46,6 +46,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	*/
 	/* class    instance      title       	 tags mask    isfloating   isterminal  noswallow  monitor */
+	{ "mpv",     NULL,       NULL,       	    -1,       0,           0,         0,        -1 },
 	{ "Gimp",     NULL,       NULL,       	    1 << 8,       0,           0,         0,        -1 },
 	{ "St",       NULL,       NULL,       	    0,            0,           1,         0,        -1 },
 	{ NULL,       NULL,       "Event Tester",   0,            0,           0,         1,        -1 },
