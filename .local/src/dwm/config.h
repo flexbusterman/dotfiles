615905992ce9ade7e3b15e07328080ec024279a2
flexbusterman <generalstrejk@gmail.com>
2020-06-16 15:45:06 +0200

    dwm attempted to launch barrier without tag

--- .local/src/dwm/config.h
+++ .local/src/dwm/config.h
@@ -47,8 +47,7 @@ static const Rule rules[] = {
    */
   /* class    instance      title          tags mask    isfloating   isterminal  noswallow  monitor */
   { "mpv",     NULL,       NULL,            -1,       0,           0,         0,        -1 },
-  /* { "Barrier",     NULL,       NULL,  1 << 8,       0,           0,         0,        -1 }, */
-  { "Barrier",     NULL,       NULL,  1 << 7,       0,           0,         0,        -1 },
+{ "Barrier",     NULL,       NULL,  1 << 7,       0,           0,         0,        -1 },
   { "St",       NULL,       NULL,             0,            0,           1,         0,        -1 },
   { NULL,       NULL,       "Event Tester",   0,            0,           0,         1,        -1 },
   { NULL,      "spterm",    NULL,             SPTAG(0),     1,           1,         0,        -1 },
