3f4ddf583c6258e16cbe122bdf304463a2a96de6
flexbusterman <generalstrejk@gmail.com>
2020-07-27 19:47:25 +0200

    "dwm gap size"

--- .local/src/dwm/config.h
+++ .local/src/dwm/config.h
@@ -3,11 +3,11 @@
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-/* default gap default 220 */
-static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 20;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 20;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 20;       /* vert outer gap between windows and screen edge */
+/* default gap default 221 */
+static const unsigned int gappih    = 21;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 21;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 21;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 21;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
