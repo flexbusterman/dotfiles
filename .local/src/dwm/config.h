82aa35cc670bd3c28f2bf5f1b6571a4f619608de
flexbusterman <generalstrejk@gmail.com>
2020-07-27 19:46:18 +0200

    "dwm gap size"

--- .local/src/dwm/config.h
+++ .local/src/dwm/config.h
@@ -4,10 +4,10 @@
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 /* default gap default 220 */
-static const unsigned int gappih    = 30;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 30;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 30;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 20;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 20;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 20;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
@@ -218,7 +218,8 @@ static Key keys[] = {
 { MODKEY,     XK_b,   togglebar,  {0} },
   /* { MODKEY|ShiftMask,    XK_b,   spawn,    SHCMD("") }, */
 { MODKEY,     XK_n,   spawn,    SHCMD("st -e nvim -c VimwikiIndex") },
-{ MODKEY|ShiftMask,   XK_n,   spawn,    SHCMD("st -e newsboat; pkill -RTMIN+6 dwmblocks") },
+// { MODKEY|ShiftMask,   XK_n,   spawn,    SHCMD("st -e newsboat; pkill -RTMIN+6 dwmblocks") },
+{ MODKEY|ShiftMask,   XK_n,   spawn,    SHCMD("st -e newsboat") },
 { MODKEY,     XK_m,   spawn,    SHCMD("st -e ncmpcpp") },
 { MODKEY|ShiftMask,   XK_m,   spawn,    SHCMD("pamixer -t; kill -44 $(pidof dwmblocks)") },
 { MODKEY,     XK_comma, spawn,    SHCMD("mpc prev") },
