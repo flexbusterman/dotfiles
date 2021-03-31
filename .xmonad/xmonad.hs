46a4d1a954ea6fadf7c4ffa1cf12008743f6aeb8
Christian Augustin <christianaugustinmusic@gmail.com>
2021-03-31 22:43:22 +0200

    "zsh as default"

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -136,7 +136,7 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     , ((modm .|. shiftMask, xK_slash ), spawn ("echo \"" ++ help ++ "\" | xmessage -file -"))
 
     -- , ((modm .|. shiftMask, xK_comma ), spawn ("alacritty -o env.SHELL=/bin/zsh -e vifmrun"))
-    , ((modm .|. shiftMask, xK_comma ), spawn ("alacritty -e vifm"))
+    , ((modm .|. shiftMask, xK_comma ), spawn ("alacritty -o env.SHELL=/bin/zsh -e vifm"))
     , ((modm .|. shiftMask, xK_period ), spawn ("qutebrowser"))
     , ((modm .|. controlMask .|. shiftMask, xK_period ), spawn ("brave"))
     , ((modm .|. shiftMask, xK_x), spawn ("xkill"))
