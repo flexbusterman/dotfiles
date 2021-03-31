22c6020041ef7c64c7b8f2086aea1101b089523e
Christian Augustin <christianaugustinmusic@gmail.com>
2021-03-31 14:01:57 +0200

    "clean"

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -20,7 +20,7 @@ import XMonad.Util.SpawnOnce
 
 -- The preferred terminal program, which is used in a binding below and by
 -- certain contrib modules.
---
+
 myTerminal      = "alacritty"
 
 -- Whether focus follows the mouse pointer.
