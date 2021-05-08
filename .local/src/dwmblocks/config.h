fcf715b4867676a532dfa282e2196617111e08bb
Christian Augustin <christianaugustinmusic@gmail.com>
2021-05-08 23:05:28 +0200

    "media buttons"

--- .local/src/dwmblocks/config.h
+++ .local/src/dwmblocks/config.h
@@ -1,37 +1,63 @@
-//Modify this file to change what commands output to your statusbar, and recompile using the make command.
-static const Block blocks[] = {
-  /*Icon*/  /*Command*/   /*Update Interval*/ /*Update Signal*/
-	// {"",  "dropbox", 5,  0},
-	//
-	// {"",	"face",	1,	16},
-	{"",	"mailbox",	5,	12},
-	{"",	"nettraf",	1,	16},
-	// {"",	"deadbeef",	10,	0},
-  {"",  "internet", 20,  4},
-	{"",  "disk", 15,  5},
-  {"",  "memory", 10, 18},
-  {"",  "cpu",    1, 18},
-	{"",	"volume",	0,	10},
-  {"",  "battery",  5,  3},
-	{"",	"clock",	60,	1},
-	/* {"",	"music",	0,	11},*/
-	// {"",	"pacpackages",	0,	8},
-	//{"",	"news",		0,	6},
-	/* {"",	"crypto",	0,	13}, */
-	//{"",	"torrent",	20,	7},
-	/* {"",	"moonphase",	18000,	17}, */
-	//{"",	"weather",	18000,	5},
-	//{"",	"help-icon",	0,	15},
-  //	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
-	/* {"",	"price bat \"Basic Attention Token\" 🦁",	0,	20}, */
-	/* {"",	"price btc Bitcoin 💰",				0,	21}, */
-	/* {"",	"price lbc \"LBRY Token\" 📚",			0,	22}, */
+/* macro for conveniently specifying pathu and pathc below */
+#define PATH(name)                      "/home/flex/.local/src/dwmblocks/blocks/"name
+
+/* buffer size for capturing output of the programs used for updating blocks */
+#define CMDOUTLENGTH                    50
+
+/* DELIMITERENDCHAR must be less than 32.
+ * At max, DELIMITERENDCHAR - 1 number of clickable blocks are allowed.
+ * Raw characters larger than DELIMITERENDCHAR and smaller than ' ' in ASCII
+   character set can be used for signaling color change in status.
+ * The character corresponding to DELIMITERENDCHAR + 1 ('\x0b' when
+   DELIMITERENDCHAR is 10) will switch the active colorscheme to the first one
+   defined in colors array in dwm's config.h and so on.
+ * If you wish to change DELIMITERENDCHAR, don't forget to update its value in
+   dwm.c and color codes in your pathu programs. */
+// #define DELIMITERENDCHAR                10
+#define DELIMITERENDCHAR                10
+
+/* delimiter specified as an array of characters
+ * don't remove DELIMITERENDCHAR at the end */
+// static const char delimiter[] = { ' ', ' ', ' ', DELIMITERENDCHAR };
+static const char delimiter[] = { '│', DELIMITERENDCHAR };
+// static char *delimiter = "│";
+
+#include "block.h"
+
+/* If interval of a block is set to 0, the block will only be updated once at
+   startup.
+ * If interval is set to a negative value, the block will never be updated in
+   the main loop.
+ * Set pathc to NULL if clickability is not required for the block.
+ * Set signal to 0 if both clickability and signaling are not required for the
+   block.
+ * Signal must be less than DELIMITERENDCHAR for clickable blocks.
+ * If multiple signals are pending, then the lowest numbered one will be
+   delivered first. */
+
+/* pathu - path of the program whose output is used as status text of the block
+           (output of the program should be a single line of text with at most
+            one newline character at the end)
+ * pathc - path of the program used for handling clicks on the block */
+
+/* 1 interval = INTERVALs seconds, INTERVALn nanoseconds */
+#define INTERVALs                       1
+#define INTERVALn                       0
+
+static Block blocks[] = {
+/*      pathu                           pathc                           interval        signal */
+        // { PATH("calendar.sh"),          NULL,                           30,             1},
+        // { PATH("volume.sh"),            PATH("volume_button.sh"),       0,              2},
+        // { PATH("cpu_temp.sh"),          PATH("cpu_temp_button.sh"),     1,              4},
+        // { PATH("battery.sh"),           PATH("battery_button.sh"),      30,             3},
+				{ PATH("mailbox"), NULL,	5,	12},
+				{ PATH("nettraf"), NULL,	1,	16},
+				{ PATH("internet"), NULL, 20,  4},
+				{ PATH("disk"), NULL, 15,  5},
+				{ PATH("memory"), NULL, 10, 18},
+				{ PATH("cpu"), NULL,    1, 18},
+				{ PATH("volume"), NULL,	0,	10},
+				{ PATH("battery"), NULL,  5,  3},
+				{ PATH("clock"), NULL,	60,	1},
+        { NULL } /* just to mark the end of the array */
 };
-
-//sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char *delim = "│";
-
-// Have dwmblocks automatically recompile and run when you edit this file in
-// vim with the following line in your vimrc/init.vim:
-
-// autocmd BufWritePost ~/.local/src/dwmblocks/config.h !cd ~/.local/src/dwmblocks/; sudo make install && { killall -q dwmblocks;setsid dwmblocks & }
