0b4e025c0c022d5382d89c9fe0a4a3bba7c5d70e
Christian Augustin <augustinsupremacy@gmail.com>
2022-05-08 13:43:26 +0200

    "normal and focus mode bindings"

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,5 +7,6 @@ killall -q polybar
 
 # Launch Polybar, using default config location ~/.config/polybar/config.ini
 polybar mybar -c $HOME/.config/polybar/config.ini 2>&1 | tee -a /tmp/polybar.log & disown
-(xdo id -m -N Polybar && polybar-msg cmd hide)&
-echo "Polybar launched..."
+
+# autohide
+(xdo id -m -N Polybar && polybar-msg cmd hide) &
