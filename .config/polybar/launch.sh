7d469fe578f548e7c424fa55cf38be03725a2bfb
Christian Augustin <augustinsupremacy@gmail.com>
2022-05-14 19:18:46 +0200

    "xmonad"

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -8,16 +8,15 @@ killall -q polybar
 # Launch Polybar, using default config location ~/.config/polybar/config.ini
 pgrep polybar || polybar mybar -c $HOME/.config/polybar/config.ini 2>&1 | tee -a /tmp/polybar.log & disown
 
-# autohide if focus variable is set
 
 # (xdo id -m -N Polybar && )
 
-focus=$(cat ~/.config/leftwm/focus | while read line; do echo $line; done)
+# focus=$(cat ~/.config/leftwm/focus | while read line; do echo $line; done)
 
-if [[ $focus == "true" ]]; then
-	wait
-	(xdo id -m -N Polybar && polybar-msg cmd hide)
-else
-	wait
-	(xdo id -m -N Polybar && polybar-msg cmd show)
-fi
+# if [[ $focus == "true" ]]; then
+	# wait
+	# (xdo id -m -N Polybar && polybar-msg cmd hide)
+# else
+	# wait
+	# (xdo id -m -N Polybar && polybar-msg cmd show)
+# fi
