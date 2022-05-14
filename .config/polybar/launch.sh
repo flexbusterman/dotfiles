aa9a32b6d47855a29fc1b311cc89c312d5183c48
Christian Augustin <augustinsupremacy@gmail.com>
2022-05-14 20:20:51 +0200

    "xmonad bindings"

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 # Terminate already running bar instances
-killall -q polybar
+pgrep polybar && killall -q polybar
 # If all your bars have ipc enabled, you can also use 
 # polybar-msg cmd quit
 
 # Launch Polybar, using default config location ~/.config/polybar/config.ini
-pgrep polybar || polybar mybar -c $HOME/.config/polybar/config.ini 2>&1 | tee -a /tmp/polybar.log & disown
-
+polybar mybar -c $HOME/.config/polybar/config.ini 2>&1 | tee -a /tmp/polybar.log & disown
 
 # (xdo id -m -N Polybar && )
 
