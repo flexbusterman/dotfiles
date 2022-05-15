3215419ef17baacd8b1ee5edc9cfb5ae91ced9ce
Christian Augustin <augustinsupremacy@gmail.com>
2022-05-15 03:29:46 +0200

    "leftwm"

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
 # Terminate already running bar instances
-pgrep polybar && killall -q polybar
+killall -q polybar
 # If all your bars have ipc enabled, you can also use 
 # polybar-msg cmd quit
 
 # Launch Polybar, using default config location ~/.config/polybar/config.ini
-polybar mybar -c $HOME/.config/polybar/config.ini 2>&1 | tee -a /tmp/polybar.log & disown
+pgrep polybar || polybar mybar -c $HOME/.config/polybar/config.ini 2>&1 | tee -a /tmp/polybar.log & disown
+
+# autohide if focus variable is set
 
 # (xdo id -m -N Polybar && )
 
-# focus=$(cat ~/.config/leftwm/focus | while read line; do echo $line; done)
+focus=$(cat ~/.config/leftwm/focus | while read line; do echo $line; done)
 
-# if [[ $focus == "true" ]]; then
-	# wait
-	# (xdo id -m -N Polybar && polybar-msg cmd hide)
-# else
-	# wait
-	# (xdo id -m -N Polybar && polybar-msg cmd show)
-# fi
+if [[ $focus == "true" ]]; then
+	wait
+	(xdo id -m -N Polybar && polybar-msg cmd hide)
+else
+	wait
+	(xdo id -m -N Polybar && polybar-msg cmd show)
+fi
