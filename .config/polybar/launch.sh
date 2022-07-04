829e1ecc60e011562eaf22083e61445b3ef52aac
Christian Augustin <augustinsupremacy@gmail.com>
2022-07-04 14:46:35 +0200

    "darkmode bindings"

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -2,20 +2,20 @@
 
 # Terminate already running bar instances
 killall -q polybar
+
 # If all your bars have ipc enabled, you can also use 
 # polybar-msg cmd quit
 
 # Launch Polybar, using default config location ~/.config/polybar/config.ini
+wait
 pgrep polybar || polybar mybar -c $HOME/.config/polybar/config.ini 2>&1 | tee -a /tmp/polybar.log & disown
 
 # autohide if focus variable is set
-
 # (xdo id -m -N Polybar && )
 
 # focus=$(cat ~/.config/leftwm/focus | while read line; do echo $line; done)
 
 # if [[ $focus == "true" ]]; then
-	# wait
 	# (xdo id -m -N Polybar && polybar-msg cmd hide)
 # else
 	# wait
