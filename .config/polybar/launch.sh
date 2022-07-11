bc72981a21773843455ef92063ed5a3cc1f89efb
Christian Augustin <augustinsupremacy@gmail.com>
2022-07-11 02:50:31 +0200

    ""

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,17 +7,22 @@ killall -q polybar
 # polybar-msg cmd quit
 
 # Launch Polybar, using default config location ~/.config/polybar/config.ini
-wait
-pgrep polybar || polybar mybar -c $HOME/.config/polybar/config.ini 2>&1 | tee -a /tmp/polybar.log & disown
+# wait
+# pgrep polybar || polybar mybar -c $HOME/.config/polybar/config.ini 2>&1 | tee -a /tmp/polybar.log & disown
+polybar mybar -c $HOME/.config/polybar/config.ini 2>&1 | tee -a /tmp/polybar.log & disown
 
 # autohide if focus variable is set
 # (xdo id -m -N Polybar && )
 
-# focus=$(cat ~/.config/leftwm/focus | while read line; do echo $line; done)
+focus=$(cat ~/.config/i3/focus | while read line; do echo $line; done)
+bar=$(cat ~/.config/i3/bar | while read line; do echo $line; done)
+notify-send "focus: $focus"
+notify-send "bar: $bar"
 
-# if [[ $focus == "true" ]]; then
-	# (xdo id -m -N Polybar && polybar-msg cmd hide)
-# else
-	# wait
-	# (xdo id -m -N Polybar && polybar-msg cmd show)
-# fi
+if [[ $focus == "true" || $bar == "false" ]]; then
+	wait
+	(xdo id -m -N Polybar && polybar-msg cmd hide)
+else
+	wait
+	(xdo id -m -N Polybar && polybar-msg cmd show)
+fi
