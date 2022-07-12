89f98db866f4c54b9d82d400291116a5eea0658c
Christian Augustin <augustinsupremacy@gmail.com>
2022-07-12 13:46:19 +0200

    "better bar handling again"

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -12,8 +12,8 @@
 
 # sleep 0.1
 
-# pgrep polybar || polybar mybar -c $HOME/.config/polybar/config.ini 2>&1 | tee -a /tmp/polybar.log 
-pidof polybar || polybar mybar -c $HOME/.config/polybar/config.ini 2>&1 | tee -a /tmp/polybar.log & disown
+pgrep polybar || polybar mybar -c $HOME/.config/polybar/config.ini 2>&1 | tee -a /tmp/polybar.log & disown
+# pidof polybar || polybar mybar -c $HOME/.config/polybar/config.ini 2>&1 | tee -a /tmp/polybar.log & disown
 
 # autohide if focus variable is set
 # (xdo id -m -N Polybar && )
