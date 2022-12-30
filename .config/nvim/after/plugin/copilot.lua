e8efb1a4ecf083a93fa7b6f0500d9424ee5178f5
Christian Augustin <christianaugustinmusic@gmail.com>
2022-12-30 23:09:13 +0100

    ""

---
+++ .config/nvim/after/plugin/copilot.lua
@@ -0,0 +1,12 @@
+vim.g.copilot_filetypes = {
+	["*"] = false,
+	["javascript"] = true,
+	["typescript"] = true,
+	["lua"] = false,
+	["rust"] = false,
+	["c"] = true,
+	["c#"] = true,
+	["c++"] = true,
+	["go"] = true,
+	["python"] = true,
+}
