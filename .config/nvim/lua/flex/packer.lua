f159d453ced1b42bdd242bb79ba8a543659d65e7
Christian Augustin <christianaugustinmusic@gmail.com>
2023-03-03 20:58:14 +0100

    "continued nvim config"

--- .config/nvim/lua/flex/packer.lua
+++ .config/nvim/lua/flex/packer.lua
@@ -16,7 +16,13 @@ return require('packer').startup(function(use)
   -- use 'Mofiqul/dracula.nvim'
   use 'phaazon/hop.nvim'
 
-  -- use({'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'})
+	use {
+			'nvim-treesitter/nvim-treesitter',
+			run = ':TSUpdate'
+	}
+
+	use("nvim-treesitter/nvim-treesitter-context")
+
   -- use('nvim-treesitter/playground')
 	--
   -- use('theprimeagen/harpoon')
@@ -47,32 +53,35 @@ return require('packer').startup(function(use)
     end
   }
 
+  use {
+	  'VonHeikemen/lsp-zero.nvim',
+	  branch = 'v1.x',
+	  requires = {
+		  -- LSP Support
+		  {'neovim/nvim-lspconfig'},
+		  {'williamboman/mason.nvim'},
+		  {'williamboman/mason-lspconfig.nvim'},
+
+		  -- Autocompletion
+		  {'hrsh7th/nvim-cmp'},
+		  {'hrsh7th/cmp-buffer'},
+		  {'hrsh7th/cmp-path'},
+		  {'saadparwaiz1/cmp_luasnip'},
+		  {'hrsh7th/cmp-nvim-lsp'},
+		  {'hrsh7th/cmp-nvim-lua'},
+
+		  -- Snippets
+		  {'L3MON4D3/LuaSnip'},
+		  {'rafamadriz/friendly-snippets'},
+	  }
+  }
+
+
   -- use('jose-elias-alvarez/null-ls.nvim')
   -- use('MunifTanjim/prettier.nvim')
 
-  -- use {
-    -- 'VonHeikemen/lsp-zero.nvim',
-    -- requires = {
-      -- -- LSP Support
-      -- {'neovim/nvim-lspconfig'},
-      -- {'williamboman/mason.nvim'},
-      -- {'williamboman/mason-lspconfig.nvim'},
-
-      -- -- Autocompletion
-      -- {'hrsh7th/nvim-cmp'},
-      -- {'hrsh7th/cmp-buffer'},
-      -- {'hrsh7th/cmp-path'},
-      -- {'saadparwaiz1/cmp_luasnip'},
-      -- {'hrsh7th/cmp-nvim-lsp'},
-      -- {'hrsh7th/cmp-nvim-lua'},
-
-      -- -- Snippets
-      -- {'L3MON4D3/LuaSnip'},
-      -- -- {'rafamadriz/friendly-snippets'},
-    -- }
-  -- }
-
   -- use("folke/zen-mode.nvim")
+	--
 	use("github/copilot.vim")
 
   use {
@@ -88,38 +97,7 @@ return require('packer').startup(function(use)
 
 	use 'echasnovski/mini.nvim'
 	require('mini.pairs').setup()
-	require('mini.comment').setup(
-
-		-- No need to copy this inside `setup()`. Will be used automatically.
-		{
-			-- Options which control module behavior
-			options = {
-				-- Whether to ignore blank lines when adding comment
-				ignore_blank_line = false,
-				-- Whether to recognize as comment only lines without indent
-				start_of_line = false,
-			},
-			-- Module mappings. Use `''` (empty string) to disable one.
-			mappings = {
-				-- Toggle comment (like `gcip` - comment inner paragraph) for both
-				-- Normal and Visual modes
-				comment = 'gc',
-				-- Toggle comment on current line
-				comment_line = '<leader>;',
-				-- Define 'comment' textobject (like `dgc` - delete whole comment block)
-				textobject = 'gc',
-			},
-			-- Hook functions to be executed at certain stage of commenting
-			hooks = {
-				-- Before successful commenting. Does nothing by default.
-				pre = function() end,
-				-- After successful commenting. Does nothing by default.
-				post = function() end,
-			},
-		}
-
-	)
+	require('mini.comment').setup({ mappings = { comment_line = '<leader>;' } })
 	require('mini.statusline').setup()
 
 end)
-
