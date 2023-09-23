64eb83198e26f6c629816b7563734c8305b197f8
Christian Augustin <christianaugustinmusic@gmail.com>
2023-09-23 17:16:48 +0200

    "restore vimwiki bindings"

--- .config/nvim/lua/flex/remap.lua
+++ .config/nvim/lua/flex/remap.lua
@@ -101,18 +101,18 @@ vim.keymap.set("n", "g]", "<cmd>lua _G.smart_tag_jump()<CR>")
 
 -- vimwiki
 -- unbinding C-m
-vim.g.vimwiki_key_mappings = {
-  all_maps = 1,
-  global = 1,
-  headers = 1,
-  text_objs = 1,
-  table_format = 1,
-  table_mappings = 1,
-  lists = 1,
-  links = 0,
-  html = 1,
-  mouse = 1,
-}
+-- vim.g.vimwiki_key_mappings = {
+--   all_maps = 1,
+--   global = 1,
+--   headers = 1,
+--   text_objs = 1,
+--   table_format = 1,
+--   table_mappings = 1,
+--   lists = 1,
+--   links = 1,
+--   html = 1,
+--   mouse = 1,
+-- }
 
 vim.keymap.set("n", "<leader>i", ":VimwikiIndex<CR>")
 
