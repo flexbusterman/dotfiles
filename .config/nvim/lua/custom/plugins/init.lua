ad10f62077242453ae82a45d2f76b0daf939ed4b
Christian Augustin <christianaugustinmusic@gmail.com>
2023-10-06 01:53:43 +0200

    "hop"

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -3,33 +3,55 @@
 --
 -- See the kickstart.nvim README for more information
 return {
-	'theprimeagen/harpoon',
-	'christoomey/vim-titlecase',
-	{
-		'echasnovski/mini.nvim',
-		config = function()
-			require('mini.base16').setup {
-				palette = {
-					base00 = "#282936", -- background
-					base01 = "#3a3c4e",
-					base02 = "#4d4f68",
-					base03 = "#626483",
-					base04 = "#62d6e8",
-					base05 = "#e9e9f4", -- foreground
-					base06 = "#f1f2f8",
-					base07 = "#f7f7fb",
-					base08 = "#ea51b2",
-					base09 = "#b45bcf",
-					base0A = "#00f769",
-					base0B = "#ebff87",
-					base0C = "#a1efe4",
-					base0D = "#62d6e8",
-					base0E = "#b45bcf",
-					base0F = "#00f769",
-				},
-				use_cterm = true,
-			}
-		end,
-	},
-	'xiyaowong/transparent.nvim'
+  'theprimeagen/harpoon',
+  'christoomey/vim-titlecase',
+  {
+    'echasnovski/mini.nvim',
+    config = function()
+      require('mini.base16').setup {
+        palette = {
+          base00 = "#282936", -- background
+          base01 = "#3a3c4e",
+          base02 = "#4d4f68",
+          base03 = "#626483",
+          base04 = "#62d6e8",
+          base05 = "#e9e9f4", -- foreground
+          base06 = "#f1f2f8",
+          base07 = "#f7f7fb",
+          base08 = "#ea51b2",
+          base09 = "#b45bcf",
+          base0A = "#00f769",
+          base0B = "#ebff87",
+          base0C = "#a1efe4",
+          base0D = "#62d6e8",
+          base0E = "#b45bcf",
+          base0F = "#00f769",
+        },
+        use_cterm = true,
+      }
+    end,
+  },
+  'xiyaowong/transparent.nvim',
+  'tidalcycles/vim-tidal',
+  {
+    'phaazon/hop.nvim',
+    branch = 'v2', -- optional but strongly recommended
+    config = function()
+      -- you can configure Hop the way you like here; see :h hop-config
+      require('hop').setup { keys = 'etovxqpdygfblzhckisuran' }
+      vim.api.nvim_set_keymap("n", "<Leader>f", "<cmd>HopWord<CR>", {noremap=true})
+    end
+  },
+  {
+    'vimwiki/vimwiki',
+    config = function()
+      vim.g.vimwiki_list = {
+        {
+          path = '/home/flex/Dropbox/NOTES/',
+          syntax = 'markdown',
+          ext = '.md',
+        }
+      }
+    end
+  }
 }
