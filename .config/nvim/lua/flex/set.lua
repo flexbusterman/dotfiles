8ebdadc7a7a958803fbd15340d0135371513af7f
Christian Augustin <christianaugustinmusic@gmail.com>
2023-10-12 01:40:45 +0200

    "organizing nvim config"

--- .config/nvim/lua/flex/set.lua
+++ .config/nvim/lua/flex/set.lua
@@ -1,61 +1,74 @@
--- vim.opt.guicursor = ""
-
--- relative numbers
+-- Make relative line numbers default
+vim.wo.number = true
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
+-- Enable mouse mode
+vim.o.mouse = 'a'
+
+-- Sync clipboard between OS and Neovim.
+vim.opt.clipboard = 'unnamedplus'
+
+-- Save undo history
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+-- searching and highlighting
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+-- Keep signcolumn on by default
+vim.opt.signcolumn = 'yes'
+
+-- Decrease update time
+vim.opt.updatetime = 50
+vim.opt.timeoutlen = 300
+
+-- Set completeopt to have a better completion experience
+vim.o.completeopt = 'menuone,noselect'
+
+-- Enable termguicolors and other visual settings
+vim.opt.termguicolors = true
+vim.opt.cursorline = true
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.filetype.plugin = "on"
+vim.opt.syntax = "on"
+vim.opt.scrolloff = 8
+vim.opt.wildignore = { '*/node_modules/*' }
+vim.opt.modeline = false
+
 -- indenting
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = false
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.shiftround = true
+vim.opt.wrap = true
+
+-- global settings
+vim.g.cmdheight = 1
+vim.g.nocompatible = true
+vim.g.base16_transparent_background = 1
+vim.opt.swapfile = false
+vim.opt.backup = false
+
+-- Commands
+vim.cmd [[ autocmd FileType vimwiki lua require('cmp').setup.buffer { enabled = false } ]]
+
+-- vim.o.breakindent = true
+-- vim.opt.guicursor = ""
 -- vim.opt.expandtab = false
 -- vim.opt.tabstop = 2
 -- vim.opt.shiftwidth = 2
 -- vim.opt.softtabstop = 2
 -- vim.opt.smarttab = true
-vim.opt.autoindent = true
--- vim.opt.smartindent = true
 -- vim.opt.shiftround = true
-
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = false
-vim.opt.smartindent = true
-vim.opt.shiftround = true
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.wrap = true
-vim.opt.swapfile = false
-vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
-vim.opt.undofile = true
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
-vim.opt.termguicolors = true
-vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
-vim.opt.isfname:append("@-@")
-vim.opt.updatetime = 50
-vim.opt.clipboard = "unnamedplus"
-vim.opt.cursorline = true
-vim.opt.splitbelow = true
-vim.opt.splitright = true
 -- vim.opt.colorcolumn = "80"
-vim.opt.filetype.plugin = "on"
-vim.g.cmdheight = 1
-vim.g.nocompatible = true
-vim.opt.syntax = "on"
-vim.opt.modeline = false
-
 -- vim.fn.setreg('s', 'dt_xllla-lla-lllr lla_lla_0j')
-
-vim.opt.wildignore = {'*/node_modules/*'}
-
--- disable completion in vimwiki
-vim.cmd [[ autocmd FileType vimwiki lua require('cmp').setup.buffer { enabled = false } ]]
-
-vim.g.nocompatible = true
-
-vim.g.base16_transparent_background = 1
-
 -- vim.cmd [[ autocmd ColorScheme * hi Normal ctermbg=none guibg=none ]]
-
 -- au ColorScheme * hi Normal ctermbg=none guibg=none
--- au ColorScheme myspecialcolors hi Normal ctermbg=red guibg=red
+-- au ColorScheme myspecialcolors hi Normal ctermbg=red guibg=red-- kickstart options
