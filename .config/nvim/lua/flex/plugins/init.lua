16c3f483129d6b9f98f54f2a69f805073397abe8
Christian Augustin <christianaugustinmusic@gmail.com>
2023-10-12 01:17:59 +0200

    "comments in remaps"

--- .config/nvim/lua/flex/plugins/init.lua
+++ .config/nvim/lua/flex/plugins/init.lua
@@ -94,7 +94,11 @@ return {
 	{ 'numToStr/Comment.nvim',
 		opts = {
 			ignore = '^$'
-		}
+		},
+		config = function()
+			vim.api.nvim_set_keymap('n', '<leader>;', '<Plug>(comment_toggle_linewise_current)', { noremap = false, silent = true })
+			vim.api.nvim_set_keymap('x', '<leader>;', '<Plug>(comment_toggle_linewise_visual)', { noremap = false, silent = true })
+		end
 	},
 
 }
