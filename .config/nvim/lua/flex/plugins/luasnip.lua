c4b96930185ffcf7f19343e2a3b410bccf7c30d0
Christian Augustin <christianaugustinmusic@gmail.com>
2023-11-21 10:28:32 +0100

    "updated scripts"

--- .config/nvim/lua/flex/plugins/luasnip.lua
+++ .config/nvim/lua/flex/plugins/luasnip.lua
@@ -2,5 +2,7 @@ return {
 	'github/copilot.vim',
 	config = function()
 		require("luasnip.loaders.from_lua").lazy_load({ paths = "~/.config/nvim/lua/flex/plugins/luasnippets/" })
+		require('luasnip').filetype_extend("javascript", { "javascriptreact" })
+		require('luasnip').filetype_extend("javascript", { "html" })
 	end
 }
