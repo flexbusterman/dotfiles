a9f9d5a5c412ff5e412a5b62caf8dacb9f343c9c
Christian Augustin <christianaugustinmusic@gmail.com>
2023-12-01 20:03:29 +0100

    ""

--- .config/nvim/lua/flex/init.lua
+++ .config/nvim/lua/flex/init.lua
@@ -1,6 +1,7 @@
 require("flex.set")
 require("flex.remap")
 require("flex.netrw")
+require("flex.plugins")
 
 local augroup = vim.api.nvim_create_augroup
 local FlexGroup = augroup('Flex', {})
@@ -9,38 +10,38 @@ local autocmd = vim.api.nvim_create_autocmd
 local yank_group = augroup('HighlightYank', {})
 
 function R(name)
-    require("plenary.reload").reload_module(name)
+	require("plenary.reload").reload_module(name)
 end
 
 autocmd('TextYankPost', {
-    group = yank_group,
-    pattern = '*',
-    callback = function()
-        vim.highlight.on_yank({
-            higroup = 'IncSearch',
-            timeout = 40,
-        })
-    end,
+	group = yank_group,
+	pattern = '*',
+	callback = function()
+		vim.highlight.on_yank({
+			higroup = 'IncSearch',
+			timeout = 40,
+		})
+	end,
 })
 
-autocmd({"BufWritePre"}, {
-    group = FlexGroup,
-    pattern = "*",
-    command = [[%s/\s\+$//e]],
+autocmd({ "BufWritePre" }, {
+	group = FlexGroup,
+	pattern = "*",
+	command = [[%s/\s\+$//e]],
 })
 
-autocmd({"BufWritePre"}, {
-    group = FlexGroup,
-		pattern = { '*.vue' },
-		command = 'silent! EslintFixAll',
+autocmd({ "BufWritePre" }, {
+	group = FlexGroup,
+	pattern = { '*.vue' },
+	command = 'silent! EslintFixAll',
 })
 
 -- vim.g.netrw_browse_split = 0
 -- vim.g.netrw_banner = 0
 -- vim.g.netrw_winsize = 25
 
-autocmd({"VimResized"}, {
-    group = FlexGroup,
-    pattern = "*",
-    command = "exec 'vertical resize ' . string(&columns *  0.5)",
+autocmd({ "VimResized" }, {
+	group = FlexGroup,
+	pattern = "*",
+	command = "exec 'vertical resize ' . string(&columns *  0.5)",
 })
--- .config/nvim/lua/flex/set.lua
+++ .config/nvim/lua/flex/set.lua
@@ -79,3 +79,22 @@ vim.cmd [[ autocmd FileType vimwiki lua require('cmp').setup.buffer { enabled = 
 -- vim.cmd [[ autocmd ColorScheme * hi Normal ctermbg=none guibg=none ]]
 -- au ColorScheme * hi Normal ctermbg=none guibg=none
 -- au ColorScheme myspecialcolors hi Normal ctermbg=red guibg=red-- kickstart options
+
+-- For some reason this isn't read from the copilot config anymore?????????????
+vim.g.copilot_assume_mapped = true
+vim.api.nvim_set_keymap("i", "<C-y>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+vim.keymap.set('i', '<C-j>', '<Nop>')
+vim.keymap.set('i', '<C-j>', '<Plug>(copilot-next)')
+vim.keymap.set('i', '<C-k>', '<Nop>')
+vim.keymap.set('i', '<C-k>', '<Plug>(copilot-previous)')
+vim.keymap.set('i', '<esc>', '<esc><Plug>(copilot-cancel)')
+vim.g.copilot_filetypes = {
+	["*"] = true,
+	["markdown"] = false,
+	["vimwiki"] = false,
+	["mail"] = false,
+	["text"] = false,
+	["supercollider"] = false,
+	["tidal"] = false,
+	["python"] = false,
+}
