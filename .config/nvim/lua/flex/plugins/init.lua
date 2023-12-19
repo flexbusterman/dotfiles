6606da010b90e2d2d89d714907ef1a91a8594807
Christian Augustin <christianaugustinmusic@gmail.com>
2023-12-19 13:28:00 +0100

    "fugitive bindings"

--- .config/nvim/lua/flex/plugins/init.lua
+++ .config/nvim/lua/flex/plugins/init.lua
@@ -29,10 +29,14 @@ return {
 	{
 		'tpope/vim-fugitive',
 		config = function()
+			-- get the change from the left
 			vim.keymap.set("n", "gh", ":diffget //2<CR>")
+			-- get the change from the right
 			vim.keymap.set("n", "gl", ":diffget //3<CR>")
-			vim.keymap.set("n", "gj", "]c")
-			vim.keymap.set("n", "gk", "[c")
+			-- jump to next change and scroll to top
+			vim.keymap.set("n", "gj", "]czt")
+			-- jump to previous change and scroll to top
+			vim.keymap.set("n", "gk", "[czt")
 		end
 	},
 	'tpope/vim-rhubarb',
