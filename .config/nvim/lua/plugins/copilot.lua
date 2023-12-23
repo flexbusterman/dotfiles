3eefafbfd5a144d4bd8fd0924990fb87008d3ff5
Christian Augustin <christianaugustinmusic@gmail.com>
2023-12-23 10:40:01 +0100

    "new bindings for cmp and copilot"

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -2,7 +2,8 @@ return {
 	'github/copilot.vim',
 	config = function()
 		vim.g.copilot_assume_mapped = true
-		vim.api.nvim_set_keymap("i", "<C-y>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+		-- vim.api.nvim_set_keymap("i", "<C-y>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+		vim.api.nvim_set_keymap("i", "<C-l>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
 		vim.keymap.set('i', '<C-j>', '<Nop>')
 		vim.keymap.set('i', '<C-j>', '<Plug>(copilot-next)')
 		vim.keymap.set('i', '<C-k>', '<Nop>')
--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -28,13 +28,12 @@ return {
 					end,
 				},
 				mapping = cmp.mapping.preset.insert({
-
 					['<C-n>'] = cmp.mapping.select_next_item(),
 					['<C-p>'] = cmp.mapping.select_prev_item(),
 					['<C-d>'] = cmp.mapping.scroll_docs(-4),
 					['<C-f>'] = cmp.mapping.scroll_docs(4),
-					['<C-Space>'] = cmp.mapping.complete {},
-					['<C-e>'] = cmp.mapping.confirm {
+					-- ['<C-Space>'] = cmp.mapping.complete {},
+					['<C-Space>'] = cmp.mapping.confirm {
 						behavior = cmp.ConfirmBehavior.Replace,
 						select = true,
 					},
