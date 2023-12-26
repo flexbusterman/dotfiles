96f2a6c9883d8bd5d702d6358db89e78743b616c
Christian Augustin <christianaugustinmusic@gmail.com>
2023-12-26 17:24:45 +0100

    "less ugly symbols"

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -64,12 +64,11 @@ return {
 			local capabilities = cmp_nvim_lsp.default_capabilities()
 
 			-- Change the Diagnostic symbols in the sign column (gutter)
-			-- (not in youtube nvim video)
-			local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-			for type, icon in pairs(signs) do
-				local hl = "DiagnosticSign" .. type
-				vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-			end
+			-- local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+			-- for type, icon in pairs(signs) do
+			-- 	local hl = "DiagnosticSign" .. type
+			-- 	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+			-- end
 
 			-- configure html server
 			lspconfig["html"].setup({
@@ -289,5 +288,5 @@ return {
 				end,
 			})
 		end,
-	}
+	},
 }
