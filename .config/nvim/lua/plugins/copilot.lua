d26976359156d95e5276e9190c562ef82f9de45e
Christian Augustin <christianaugustinmusic@gmail.com>
2023-12-29 04:20:40 +0100

    "bindings"

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -3,11 +3,11 @@ return {
 	config = function()
 		vim.g.copilot_assume_mapped = true
 		-- vim.api.nvim_set_keymap("i", "<C-y>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
-		vim.api.nvim_set_keymap("i", "<C-l>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
-		vim.keymap.set("i", "<C-j>", "<Nop>")
-		vim.keymap.set("i", "<C-j>", "<Plug>(copilot-next)")
-		vim.keymap.set("i", "<C-k>", "<Nop>")
-		vim.keymap.set("i", "<C-k>", "<Plug>(copilot-previous)")
+		vim.api.nvim_set_keymap("i", "<C-Space>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+		vim.keymap.set("i", "<C-n>", "<Nop>")
+		vim.keymap.set("i", "<C-n>", "<Plug>(copilot-next)")
+		vim.keymap.set("i", "<C-p>", "<Nop>")
+		vim.keymap.set("i", "<C-p>", "<Plug>(copilot-previous)")
 		vim.keymap.set("i", "<esc>", "<esc><Plug>(copilot-cancel)")
 		vim.g.copilot_filetypes = {
 			["*"] = true,
