36de5894c1214096e333641822568712707221d1
Christian Augustin <christianaugustinmusic@gmail.com>
2023-12-31 14:36:30 +0100

    "removing unwanted whitespace and duplicate empty lines"

--- .config/nvim/lua/autocmd.lua
+++ .config/nvim/lua/autocmd.lua
@@ -1,32 +1,40 @@
 local autocmd = vim.api.nvim_create_autocmd
-local FlexGroup = vim.api.nvim_create_augroup('Flex', {})
-
+local FlexGroup = vim.api.nvim_create_augroup("Flex", {})
 
 function R(name)
 	require("plenary.reload").reload_module(name)
 end
 
 autocmd({
-    "BufWritePre"
+	"BufWritePre",
 }, {
-    group = FlexGroup,
-    pattern = "*",
-    callback = function()
-        -- Save current cursor position
-        local save_cursor = vim.api.nvim_win_get_cursor(0)
-        -- Remove trailing whitespace from each line
-        vim.cmd([[ %s/\s\+$//e ]])
-        -- Remove trailing empty or whitespace-only lines
-        vim.cmd([[ %s/\(\n\s*\)\+\%$//e ]])
-        -- Restore cursor position
-        vim.api.nvim_win_set_cursor(0, save_cursor)
-    end
+	group = FlexGroup,
+	pattern = "*",
+	callback = function()
+		-- Save current cursor position
+		local save_cursor = vim.api.nvim_win_get_cursor(0)
+		-- https://vim.fandom.com/wiki/Remove_unwanted_empty_lines
+		-- The following will delete all trailing whitespace from each line, then replace three or more consecutive line endings with two line endings (a single blank line)
+		vim.cmd([[ %s/\s\+$//e]])
+		vim.cmd([[ %s/\n\{3,}/\r\r/e]])
+		-- Remove trailing empty lines from the document
+		vim.cmd([[ %s/\(\n\s*\)\+\%$//e ]])
+		-- Get the total number of lines in the current buffer
+		local total_lines = vim.api.nvim_buf_line_count(0)
+		-- Check if the saved cursor line is within the new buffer range
+		if save_cursor[1] > total_lines then
+			-- If not, set the cursor to the last line of the buffer
+			save_cursor[1] = total_lines
+		end
+		-- Restore cursor position
+		vim.api.nvim_win_set_cursor(0, save_cursor)
+	end,
 })
 
 autocmd({ "BufWritePre" }, {
 	group = FlexGroup,
-	pattern = { '*.vue' },
-	command = 'silent! EslintFixAll',
+	pattern = { "*.vue" },
+	command = "silent! EslintFixAll",
 })
 
 autocmd({ "VimResized" }, {
@@ -37,11 +45,11 @@ autocmd({ "VimResized" }, {
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
-local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
-vim.api.nvim_create_autocmd('TextYankPost', {
+local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+vim.api.nvim_create_autocmd("TextYankPost", {
 	callback = function()
 		vim.highlight.on_yank()
 	end,
 	group = highlight_group,
-	pattern = '*',
+	pattern = "*",
 })
