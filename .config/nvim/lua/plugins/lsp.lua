fe0523bbf7b83b02da4d2fc428674f0a4bb6558f
Christian Augustin <christianaugustinmusic@gmail.com>
2024-01-15 16:02:59 +0100

    ""

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,9 +1,26 @@
 return {
 	{
 		"williamboman/mason.nvim",
+		dependencies = {
+			"WhoIsSethDaniel/mason-tool-installer.nvim",
+		},
 		lazy = false,
 		config = function()
-			require("mason").setup()
+			local mason = require("mason")
+
+			local mason_tool_installer = require("mason-tool-installer")
+
+			mason.setup()
+			mason_tool_installer.setup({
+				ensure_installed = {
+					"prettier",
+					"stylua",
+					"isort",
+					"black",
+					"pylint",
+					"eslint_d",
+				},
+			})
 		end,
 	},
 	{
