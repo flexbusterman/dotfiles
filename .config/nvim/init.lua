bcfc0db94175af0d7fbd5c355b9eed4a0bf6ef04
Christian Augustin <christianaugustinmusic@gmail.com>
2024-03-09 03:14:33 +0100

    "plugins"

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -630,6 +630,44 @@ require("lazy").setup({
 			--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 			local servers = {
 
+				emmet_language_server = {
+					settings = {
+						filetypes = {
+							"css",
+							"eruby",
+							"html",
+							"javascript",
+							"javascriptreact",
+							"less",
+							"sass",
+							"scss",
+							"pug",
+							"typescriptreact",
+							"php",
+						},
+						-- Read more about this options in the [vscode docs](https://code.visualstudio.com/docs/editor/emmet#_emmet-configuration).
+						-- **Note:** only the options listed in the table are supported.
+						init_options = {
+							--- @type string[]
+							excludeLanguages = {},
+							--- @type string[]
+							extensionsPath = {},
+							--- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/preferences/)
+							preferences = {},
+							--- @type boolean Defaults to `true`
+							showAbbreviationSuggestions = true,
+							--- @type "always" | "never" Defaults to `"always"`
+							showExpandedAbbreviation = "always",
+							--- @type boolean Defaults to `false`
+							showSuggestionsAsSnippets = true,
+							--- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/syntax-profiles/)
+							syntaxProfiles = {},
+							--- @type table<string, string> [Emmet Docs](https://docs.emmet.io/customization/snippets/#variables)
+							variables = {},
+						},
+					},
+				},
+
 				intelephense = {
 					settings = {
 						intelephense = {
