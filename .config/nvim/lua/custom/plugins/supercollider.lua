a5607e8fa067f7f0f89b9945dbd1a8f42fe9d48d
Christian Augustin <johnchristianaugustin@gmail.com>
2024-04-23 17:02:02 +0200

    script to open google calendar in qutebrowser

--- .config/nvim/lua/custom/plugins/supercollider.lua
+++ .config/nvim/lua/custom/plugins/supercollider.lua
@@ -1,113 +1,120 @@
 return {
-  'davidgranstrom/scnvim',
-  -- dir = "/home/flex/.local/src/scnvim/",
-  ft = { 'supercollider' },
-  dependencies = {
-    'davidgranstrom/scnvim-tmux',
-  },
-  config = function()
-    local scnvim = require 'scnvim'
-    local map = scnvim.map
-    local map_expr = scnvim.map_expr
+	"davidgranstrom/scnvim",
+	-- dir = "/home/flex/.local/src/scnvim/",
+	ft = { "supercollider" },
+	dependencies = {
+		"davidgranstrom/scnvim-tmux",
+	},
+	config = function()
+		local scnvim = require("scnvim")
+		local map = scnvim.map
+		local map_expr = scnvim.map_expr
 
-    scnvim.setup {
-      ensure_installed = true,
-      sclang = {
-        cmd = nil,
-        args = {},
-      },
-      documentation = {
-        cmd = '/usr/bin/pandoc',
-        horizontal = false,
-        direction = 'right',
-        keymaps = true,
-      },
-      editor = {
-        force_ft_supercollider = true,
-        highlight = {
-          color = 'TermCursor',
-          type = 'flash',
-          flash = {
-            duration = 30,
-            repeats = 1,
-          },
-          fade = {
-            duration = 30,
-          },
-        },
-        signature = {
-          float = true,
-          auto = true,
-        },
-      },
-      snippet = {
-        engine = {
-          name = 'luasnip',
-          options = {
-            descriptions = true,
-          },
-        },
-      },
-      statusline = {
-        poll_interval = 1,
-      },
-      extensions = {
-        tmux = {
-          path = vim.fn.tempname(),
-          horizontal = true,
-          size = '20%',
-          cmd = 'scnvim-tmux',
-          args = { '$1' },
-        },
-      },
-      keymaps = {
-        ['<M-e>'] = map('editor.send_line', { 'i', 'n' }),
-        ['<CR>'] = {
-          map('editor.send_block', 'n'),
-          map('editor.send_selection', 'x'),
-        },
-        ['<leader><CR>'] = map('postwin.toggle', 'n'),
-        ['<M-CR>'] = map('postwin.toggle', 'i'),
-        ['<leader>c'] = map('postwin.clear', 'n'),
-        ['<K>'] = map('signature.show', 'n'),
-        ['<leader>.'] = map('sclang.hard_stop', { 'n', 'x' }),
-        ['<leader>s'] = map 'sclang.start',
-        ['<leader>sk'] = map 'sclang.recompile',
-        ['<F1>'] = map_expr 's.boot',
-        ['<F2>'] = map_expr 's.meter',
-      },
-    }
+		scnvim.setup({
+			ensure_installed = true,
+			sclang = {
+				cmd = nil,
+				args = {},
+			},
+			documentation = {
+				cmd = "/usr/bin/pandoc",
+				horizontal = false,
+				direction = "right",
+				keymaps = true,
+			},
+			editor = {
+				force_ft_supercollider = true,
+				highlight = {
+					color = "TermCursor",
+					type = "flash",
+					flash = {
+						duration = 30,
+						repeats = 1,
+					},
+					fade = {
+						duration = 30,
+					},
+				},
+				signature = {
+					float = true,
+					auto = true,
+				},
+			},
+			snippet = {
+				engine = {
+					name = "luasnip",
+					options = {
+						descriptions = true,
+					},
+				},
+			},
+			statusline = {
+				poll_interval = 1,
+			},
+			extensions = {
+				tmux = {
+					path = vim.fn.tempname(),
+					horizontal = true,
+					size = "30%",
+					cmd = "tail",
+					args = { "-F", "$1" },
+				},
+				-- tmux = {
+				--   path = vim.fn.tempname(),
+				--   horizontal = true,
+				--   size = '20%',
+				--   cmd = 'scnvim-tmux',
+				--   args = { '$1' },
+				-- },
+			},
+			keymaps = {
+				["<M-e>"] = map("editor.send_line", { "i", "n" }),
+				["<CR>"] = {
+					map("editor.send_block", "n"),
+					map("editor.send_selection", "x"),
+				},
+				["<leader><CR>"] = map("postwin.toggle", "n"),
+				["<M-CR>"] = map("postwin.toggle", "i"),
+				["<leader>c"] = map("postwin.clear", "n"),
+				["<K>"] = map("signature.show", "n"),
+				["<leader>."] = map("sclang.hard_stop", { "n", "x" }),
+				["<leader>s"] = map("sclang.start"),
+				["<leader>sk"] = map("sclang.recompile"),
+				["<F1>"] = map_expr("s.boot"),
+				["<F2>"] = map_expr("s.meter"),
+			},
+		})
 
-    scnvim.load_extension 'tmux'
+		scnvim.load_extension("tmux")
 
-    vim.g.scnvim_snippet_format = 'luasnip'
+		vim.g.scnvim_snippet_format = "luasnip"
 
-    -- vim.cmd([[
-    -- augroup SuperColliderSnippets
-    --     autocmd!
-    --     autocmd FileType supercollider lua require("luasnip").add_snippets("supercollider", require("scnvim/utils").get_snippets())
-    -- augroup END
-    -- ]])
-    vim.g.scnvim_snippets_loaded = false
+		-- vim.cmd([[
+		-- augroup SuperColliderSnippets
+		--     autocmd!
+		--     autocmd FileType supercollider lua require("luasnip").add_snippets("supercollider", require("scnvim/utils").get_snippets())
+		-- augroup END
+		-- ]])
+		vim.g.scnvim_snippets_loaded = false
 
-    function load_scnvim_snippets()
-      if not vim.g.scnvim_snippets_loaded then
-        require('luasnip').add_snippets('supercollider', require('scnvim/utils').get_snippets())
-        vim.g.scnvim_snippets_loaded = true
-      end
-    end
+		function load_scnvim_snippets()
+			if not vim.g.scnvim_snippets_loaded then
+				require("luasnip").add_snippets("supercollider", require("scnvim/utils").get_snippets())
+				vim.g.scnvim_snippets_loaded = true
+			end
+		end
 
-    vim.cmd [[
+		vim.cmd([[
         augroup SuperColliderSnippets
             autocmd!
             autocmd FileType supercollider lua load_scnvim_snippets()
         augroup END
-        ]]
+        ]])
 
-    -- supercollider argument transformation
+		-- supercollider argument transformation
 
-    vim.api.nvim_exec(
-      [[
+		vim.api.nvim_exec(
+			[[
 		function! ProcessRange() range
 			" Substitution command
 			'<,'>s/\(\\[^.]*\)\.[ka]r(\([^)]*\))/\r\1, \2,\r/g
@@ -136,9 +143,9 @@ return {
 			'<,'>s/\v-([0-9]+)/(-\1)/g
 		endfunction
 		]],
-      false
-    )
+			false
+		)
 
-    vim.keymap.set('v', '<leader>p', ":'<,'>call ProcessRange()<CR>")
-  end,
+		vim.keymap.set("v", "<leader>p", ":'<,'>call ProcessRange()<CR>")
+	end,
 }
