0af084fd52d1930ab9e77fdf225dc3d46fcbe26f
Christian Augustin <johnchristianaugustin@gmail.com>
2024-06-24 19:47:04 +0200

    trashing

--- .config/nvim/lua/custom/plugins/vimwiki.lua
+++ .config/nvim/lua/custom/plugins/vimwiki.lua
@@ -1,13 +1,15 @@
 return {
-	'vimwiki/vimwiki',
+	"vimwiki/vimwiki",
 	config = function()
+		vim.o.wrap = true
+		vim.o.linebreak = true
 		vim.keymap.set("n", "<leader>i", ":VimwikiIndex<CR>")
 		vim.g.vimwiki_list = {
 			{
-				path = '/home/flex/Dropbox/NOTES/',
-				syntax = 'markdown',
-				ext = '.md',
-			}
+				path = "/home/flex/Dropbox/NOTES/",
+				syntax = "markdown",
+				ext = ".md",
+			},
 		}
-	end
+	end,
 }
