7064e6ad175d21b19bd3c9cee45fb8a45718205e
Christian Augustin <johnchristianaugustin@gmail.com>
2024-06-25 05:16:07 +0200

    normal movement

--- .config/nvim/lua/custom/plugins/vimwiki.lua
+++ .config/nvim/lua/custom/plugins/vimwiki.lua
@@ -3,12 +3,12 @@ return {
 	config = function()
 		vim.o.wrap = true
 		vim.o.linebreak = true
-		vim.keymap.set("n", "j", "gj")
-		vim.keymap.set("n", "k", "gk")
-		vim.keymap.set("n", "h", "gh")
-		vim.keymap.set("n", "l", "gl")
-		vim.keymap.set("n", "0", "g0")
-		vim.keymap.set("n", "$", "g$")
+		-- vim.keymap.set("n", "j", "gj")
+		-- vim.keymap.set("n", "k", "gk")
+		-- vim.keymap.set("n", "h", "gh")
+		-- vim.keymap.set("n", "l", "gl")
+		-- vim.keymap.set("n", "0", "g0")
+		-- vim.keymap.set("n", "$", "g$")
 		vim.keymap.set("n", "<leader>i", ":VimwikiIndex<CR>")
 		vim.g.vimwiki_list = {
 			{
