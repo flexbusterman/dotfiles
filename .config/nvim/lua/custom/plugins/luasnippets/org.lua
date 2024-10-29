abe55e8ceacada5670c4f967cbeee5244eadf321
Christian Augustin <johnchristianaugustin@gmail.com>
2024-10-29 09:34:18 +0100

    orgmode snippet for code block

---
+++ .config/nvim/lua/custom/plugins/luasnippets/org.lua
@@ -0,0 +1,14 @@
+local ls = require("luasnip")
+local s = ls.snippet
+local i = ls.insert_node
+local t = ls.text_node
+local snippets = {
+	s({ trig = "code", name = "Code Block" }, {
+		t("#+BEGIN_SRC "),
+		i(1, "language"),
+		t({ "", "" }),
+		i(2, ""),
+		t({ "", "#+END_SRC" }), -- Closing line with a newline above
+	}),
+}
+return snippets
