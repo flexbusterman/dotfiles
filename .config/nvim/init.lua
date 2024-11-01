a3237ae2c12a500b5f00348950ab299ee37a2cec
Christian Augustin <johnchristianaugustin@gmail.com>
2024-11-01 21:40:27 +0100

    added lua lsp globals vim and reaper

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -897,6 +897,12 @@ require("lazy").setup({
 							},
 							-- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
 							-- diagnostics = { disable = { 'missing-fields' } },
+
+							diagnostics = {
+								-- enable": true,
+								globals = { "vim", "reaper" },
+								disable = { "lowercase-global" },
+							},
 						},
 					},
 				},
