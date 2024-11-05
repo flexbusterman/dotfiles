f3cc299f6419d5fe2acacbb5f86ed828d726141d
Christian Augustin <johnchristianaugustin@gmail.com>
2024-11-05 13:25:45 +0100

    cleaner nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,23 +1,22 @@
--- NOTE: nvim config based on kickstart
+-- nvim config based on kickstart.nvim
 -- https://github.com/nvim-lua/kickstart.nvim
 
 -- reading files in lua folder
 require("options")
 require("keymaps")
 require("autocmd")
+require("health")
 
--- [[ Install `lazy.nvim` plugin manager ]]
+-- Install `lazy.nvim` plugin manager
 -- See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
 	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
 	vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-end ---@diagnostic disable-next-line: undefined-field
+end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-	-- require 'kickstart.plugins.debug',
-	require("kickstart.plugins.indent_line"),
 	-- reading files in lua/plugins folder
 	{ import = "plugins" },
 }, {
@@ -25,6 +24,3 @@ require("lazy").setup({
 		notify = false,
 	},
 })
-
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
