14ad0dd08c34109f9ca3b8c6af61215d9478dd17
Christian Augustin <johnchristianaugustin@gmail.com>
2025-01-09 23:49:27 +0100

    switched to vimwiki markdown

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -9,6 +9,6 @@ return {
 		vim.keymap.set("i", "<c-p>", "<Plug>(copilot-previous)")
 		vim.keymap.set("n", "<c- >", "<Plug>(copilot-suggest)")
 		vim.g.copilot_no_tab_map = true
-		vim.g.copilot_filetypes = { org = false, supercollider = false, taskedit = false }
+		vim.g.copilot_filetypes = { org = false, supercollider = false, taskedit = false, markdown = false }
 	end,
 }
