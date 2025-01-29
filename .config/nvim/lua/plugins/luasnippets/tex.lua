a30a4ec93ed35bdd26ee75ec47d5bb1efc4ab22d
Christian Augustin <johnchristianaugustin@gmail.com>
2025-01-29 17:13:51 +0100

    snippets and zathura recolor

--- .config/nvim/lua/plugins/luasnippets/tex.lua
+++ .config/nvim/lua/plugins/luasnippets/tex.lua
@@ -45,5 +45,71 @@ local snippets = {
 	),
 
 	s({ trig = "pq", name = "PQ-formeln" }, { t("x &= -\\frac{p}{2} \\pm \\sqrt{\\left(\\frac{p}{2}\\right)^2 - q}") }),
+
+	s(
+		"maths",
+		fmt(
+			[[
+\documentclass{article}
+\usepackage[fleqn]{amsmath}
+\usepackage{enumitem, hyperref}
+\title{Matematik 2c}
+\date{\today}
+\author{Christian Augustin}
+\begin{document}
+\maketitle
+
+\href{https://fredrikfilmer.wixsite.com/filmer/matematik-5000-2b}{Lösningar}
+
+\section{}
+\begin{align*}
+	<> \\
+\end{align*}
+
+\end{document}
+  ]],
+			{
+				i(1, ""),
+			},
+			{
+				delimiters = "<>",
+			}
+		)
+	),
+
+	s(
+		"cases",
+		fmt(
+			[[
+	\begin{cases}
+		x_1 & <> \\
+		x_2 & <> \\
+	\end{cases}
+  ]],
+			{
+				i(1, ""),
+				i(2, ""),
+			},
+			{
+				delimiters = "<>",
+			}
+		)
+	),
+
+	s(
+		"nth root python",
+		fmt(
+			[[
+print(pow(<>, 1/<>))
+  ]],
+			{
+				i(1, "1"),
+				i(2, "1"),
+			},
+			{
+				delimiters = "<>",
+			}
+		)
+	),
 }
 return snippets
