9e4b6ca5a7eadc416de3f2b3455ee7c2d6f1097f
Christian Augustin <johnchristianaugustin@gmail.com>
2025-03-14 06:00:10 +0100

    plugins

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -1,17 +1,1 @@
-require("bookmarks"):setup({
-	last_directory = { enable = true, persist = true },
-	persist = "all",
-	desc_format = "full",
-	file_pick_mode = "hover",
-	notify = {
-		enable = false,
-		timeout = 1,
-		message = {
-			new = "New bookmark '<key>' -> '<folder>'",
-			delete = "Deleted bookmark in '<key>'",
-			delete_all = "Deleted all bookmarks",
-		},
-	},
-})
-
-require("relative-motions"):setup({ show_numbers = "relative_absolute", show_motion = true })
+require("relative-motions"):setup({ show_numbers = "relative", show_motion = true, enter_mode = "first" })
