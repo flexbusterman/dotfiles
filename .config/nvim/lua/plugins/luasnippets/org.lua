75efd62725d70bfe9994e792b6204eb9ebe65ce2
Christian Augustin <johnchristianaugustin@gmail.com>
2025-04-14 16:40:29 +0200

    nvim main editor and emacs for orgmode

--- .config/nvim/lua/plugins/luasnippets/org.lua
+++ .config/nvim/lua/plugins/luasnippets/org.lua
@@ -3,7 +3,7 @@ local s = ls.snippet
 local i = ls.insert_node
 local t = ls.text_node
 local snippets = {
-	s({ trig = "code", name = "Code Block" }, {
+	s({ trig = "code block", name = "Code Block" }, {
 		t("#+BEGIN_SRC "),
 		i(1, "language"),
 		t({ "", "" }),
